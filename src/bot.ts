9b59e653e118db0ef64877d5d6d09747b309e18c
lm9 <depkey@me.com>
2018-03-31 16:31:05 +0900

    parachuteのmoduleを外部から一度に読み込めるようにした

--- src/bot.ts
+++ src/bot.ts
@@ -2,23 +2,18 @@ import Parachute from './Parachute';
 import * as fs from 'fs-extra';
 import { Client, Message, Collection, Member } from 'eris';
 
-// parachute modules
-import auto_grouping = require('./auto_grouping');
-import ping_pong = require('./ping_pong');
-import stop = require('./stop');
-
 const keys = JSON.parse(fs.readFileSync('./confs/keys.json', 'utf8'));
 const settings = JSON.parse(fs.readFileSync('./confs/settings.json', 'utf8'));
+const modules = JSON.parse(fs.readFileSync('./confs/modules.json', 'utf8'));
 
 const token = keys['token'];
 const owner = settings['owner'];
 const prefix = settings['command_prefix'];
 
 const parachute = new Parachute(token, owner, prefix);
-
-// ループでやりてえなあこれ
-parachute.register_command(auto_grouping.label, auto_grouping.command, auto_grouping.permission);
-parachute.register_command(ping_pong.label, ping_pong.command, ping_pong.permission);
-parachute.register_command(stop.label, stop.command, stop.permission);
+modules.forEach((moduleFile: string) => {
+  const parachuteModule: {label: string, command: Function, permission: Parachute.Permission} = require(moduleFile);
+  parachute.register_command(parachuteModule.label, parachuteModule.command, parachuteModule.permission);
+});
 
 export = parachute;
