736fc7202b2a77ccb990b16e2e571c606dc9f703
lm9 <depkey@me.com>
2018-04-04 12:10:14 +0900

    ParachuteModule化

--- src/modules/stop.ts
+++ src/modules/stop.ts
@@ -1,8 +1,20 @@
 import { Client, Message, Collection, Member } from "eris";
-import { Permission } from "../parachute";
+import { Permission, ParachuteModule } from "../parachute";
 
-function stop(client: Client, message: Message, args: string[] = []) {
-  client.disconnect({ reconnect: false });
+class Stop implements ParachuteModule{
+  readonly label: string = "stop";
+  readonly permission: Permission = Permission.OWNER;
+  readonly name: string = "Stop";
+  private client?: Client;
+
+  run(message: Message, args: string[] = []) {
+    if (this.client) this.client.disconnect({ reconnect: false });
+  }
+
+  setup(client: Client) {
+    this.client = client;
+  }
+
 }
 
-export = { label: "stop", command: stop, permission: Permission.OWNER };
+export = { label: "stop", command: new Stop(), permission: Permission.OWNER };
