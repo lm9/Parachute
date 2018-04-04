3caca9cfe55c777a887b3d918e82ce61f83e2238
lm9 <depkey@me.com>
2018-04-04 13:18:33 +0900

    起動した時間を覚えておくように（そのままだけど

--- src/modules/info.ts
+++ src/modules/info.ts
@@ -5,10 +5,11 @@ export default class Info extends ParachuteModule {
   readonly label: string = "info";
   readonly permission: Permission = Permission.USER;
   readonly name: string = "Info";
-  private called_count: { [key: string]: number } = {};
+  private waked_at: Date;
 
   constructor(client: Client) {
     super(client);
+    this.waked_at = new Date();
   }
 
   public run(message: Message, args: string[] = []) {
@@ -16,6 +17,7 @@ export default class Info extends ParachuteModule {
       message.channel.createMessage(`
 Parachute is a Noob Discord Bot.
 Repository: https://github.com/lm9/Parachute
+Woke at ${this.waked_at}
       `);
     } catch (e) {
       console.error(e);
