09fb7a3eeb339fcdf43b2ab518588c1b9c587004
lm9 <depkey@me.com>
2018-04-05 22:13:11 +0900

    chumaceraをいれます

--- src/plugins/pubg.ts
+++ src/plugins/pubg.ts
@@ -1,18 +1,18 @@
 import { Client, Message, Collection, Member } from "eris";
 import { Permission, Plugin } from "../parachute";
-import * as PUBGAPI from "./lib/pubgapi/client";
+import * as Chumacera from "chumacera";
 import { readFileSync, read } from "fs-extra";
 
 export default class PUBG extends Plugin {
 	readonly label: string = "pubg";
 	readonly permission: Permission = Permission.USER;
 	readonly name: string = "PUBG";
-	readonly pubgclient: PUBGAPI.Client;
+	readonly pubgclient: Chumacera.Client;
 
 	constructor(client: Client) {
 		super(client);
-		const key:string = JSON.parse(readFileSync("./confs/keys.json", "utf8"))["plugins"]["PUBG"]["API_KEY"];
-		this.pubgclient = new PUBGAPI.Client(key);
+		const key: string = JSON.parse(readFileSync("./confs/keys.json", "utf8"))["plugins"]["PUBG"]["API_KEY"];
+		this.pubgclient = new Chumacera.Client(key);
 	}
 
 	public run(message: Message, args: string[] = []) {
@@ -22,21 +22,23 @@ export default class PUBG extends Plugin {
 			if (args[0] === "lookup" && args[1]) {
 				const region = "pc-jp";
 				new Promise<string>((resolve, reject) => {
-					this.pubgclient.getPlayers(region, { names: [args[1]] })
-					.then((players) => {
-						resolve(
-							JSON.stringify(players[0])
-						);
-					}).catch(err => {
-						reject(err);
+					this.pubgclient
+						.getPlayers(region, { names: [args[1]] })
+						.then(players => {
+							resolve(JSON.stringify(players[0]));
+						})
+						.catch(err => {
+							reject(err);
+						});
+				})
+					.then(mes => {
+						try {
+							message.channel.createMessage(mes);
+						} catch (e) {
+							console.log(e);
+						}
 					})
-				}).then((mes) => {
-					try {
-						message.channel.createMessage(mes);
-					} catch (e) {
-						console.log(e);
-					}
-				}).catch(e => console.log(e));
+					.catch(e => console.log(e));
 			}
 		}
 	}
